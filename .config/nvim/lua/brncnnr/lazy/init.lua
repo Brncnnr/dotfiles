8d3603fb87a1e888e37323d1abe3df9178e633dc
brncnnr <bri4n0c0nn0r@web.de>
2024-03-06 15:01:13 +0100

    telescope update
    implemented telescope keybindings from kickstarter, added autocompletion and format on save

--- .config/nvim/lua/brncnnr/lazy/init.lua
+++ .config/nvim/lua/brncnnr/lazy/init.lua
@@ -1,36 +1,62 @@
 return {
-    {
-        "catppuccin/nvim",
-        lazy = false,
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup({
-                --                   custom_highlights = function(colors)
-                --                       return {
-                --                           TabLineSel = {
-                --                               bg = colors.pink
-                --                           },
-                --                           Cursor = {
-                --                               bg = :
-                --                               fg = colors.pink
-                --                           }
-                --                       }
-                --                   end
-            })
-            vim.cmd.colorscheme("catppuccin-macchiato")
-        end,
-    },
-    --    {
-    --        "shaunsingh/nord.nvim",
-    --        config = function()
-    --            require("nord").set()
-    --        end
-    --    },
-    {
-        "nvim-lua/plenary.nvim",
-    },
-    {
-        "ThePrimeagen/vim-be-good",
-    },
+	--{
+	--    "catppuccin/nvim",
+	--    lazy = false,
+	--    name = "catppuccin",
+	--    priority = 1000,
+	--    config = function()
+	--        require("catppuccin").setup({
+	--            --                   custom_highlights = function(colors)
+	--            --                       return {
+	--            --                           TabLineSel = {
+	--            --                               bg = colors.pink
+	--            --                           },
+	--            --                           Cursor = {
+	--            --                               bg = :
+	--            --                               fg = colors.pink
+	--            --                           }
+	--            --                       }
+	--            --                   end
+	--        })
+	--        vim.cmd.colorscheme("catppuccin-macchiato")
+	--    end,
+	--},
+	--    {
+	--        "shaunsingh/nord.nvim",
+	--        config = function()
+	--            require("nord").set()
+	--        end
+	--    },
+	{
+		"nvim-lua/plenary.nvim",
+	},
+	{
+		"ThePrimeagen/vim-be-good",
+	},
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- Load the colorscheme here.
+			-- Like many other themes, this one has different styles, and you could load
+			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+			vim.cmd.colorscheme("tokyonight-night")
+
+			-- You can configure highlights by doing something like
+			vim.cmd.hi("Comment gui=none")
+		end,
+	},
+	{
+		"mbbill/undotree",
+		config = function()
+			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+		end,
+	},
 }
--- .config/nvim/lua/brncnnr/lazy/telescope.lua
+++ .config/nvim/lua/brncnnr/lazy/telescope.lua
@@ -5,6 +5,19 @@ return {
 
 	dependencies = {
 		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+
+			-- `build` is used to run some command when the plugin is installed/updated.
+			-- This is only run then, not every time Neovim starts up.
+			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+
+			-- `cond` is a condition used to determine whether this plugin should be
+			-- installed and loaded.
+			cond = function()
+				return vim.fn.executable("make") == 1
+			end,
+		},
 	},
 
 	config = function()
@@ -24,6 +37,8 @@ return {
 		--        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 		--        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 		vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+
+		-- need examplet/ explanation here or take standard keymaps maybe?
 		vim.keymap.set("n", "<leader>pws", function()
 			local word = vim.fn.expand("<cword>")
 			builtin.grep_string({ search = word })
@@ -35,6 +50,77 @@ return {
 		vim.keymap.set("n", "<leader>ps", function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
 		end)
+		-- [v]iew[h]elp
 		vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+
+		-- Slightly advanced example of overriding default behavior and theme
+		vim.keymap.set("n", "<leader>/", function()
+			-- You can pass additional configuration to telescope to change theme, layout, etc.
+			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+				winblend = 10,
+				previewer = false,
+			}))
+		end, { desc = "[/] Fuzzily search in current buffer" })
+
+		-- Shortcut for searching your neovim configuration files
+		vim.keymap.set("n", "<leader>sn", function()
+			builtin.find_files({ cwd = vim.fn.stdpath("config") })
+		end, { desc = "[S]earch [N]eovim files" })
+
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+			callback = function(event)
+				-- NOTE: Remember that lua is a real programming language, and as such it is possible
+				-- to define small helper and utility functions so you don't have to repeat yourself
+				-- many times.
+				--
+				-- In this case, we create a function that lets us more easily define mappings specific
+				-- for LSP related items. It sets the mode, buffer and description for us each time.
+				local map = function(keys, func, desc)
+					vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+				end
+
+				-- Jump to the definition of the word under your cursor.
+				--  This is where a variable was first declared, or where a function is defined, etc.
+				--  To jump back, press <C-T>.
+				map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+
+				---- Find references for the word under your cursor.
+				map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+
+				---- Jump to the implementation of the word under your cursor.
+				----  Useful when your language has ways of declaring types without an actual implementation.
+				map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+
+				---- Jump to the type of the word under your cursor.
+				----  Useful when you're not sure what type a variable is and you want to see
+				----  the definition of its *type*, not where it was *defined*.
+				map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+
+				---- Fuzzy find all the symbols in your current document.
+				----  Symbols are things like variables, functions, types, etc.
+				map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+
+				---- Fuzzy find all the symbols in your current workspace
+				----  Similar to document symbols, except searches over your whole project.
+				map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+				---- Rename the variable under your cursor
+				----  Most Language Servers support renaming across files, etc.
+				map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+
+				-- Execute a code action, usually your cursor needs to be on top of an error
+				-- or a suggestion from your LSP for this to activate.
+				map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+
+				-- Opens a popup that displays documentation about the word under your cursor
+				--  See `:help K` for why this keymap
+				map("K", vim.lsp.buf.hover, "Hover Documentation")
+
+				-- WARN: This is not Goto Definition, this is Goto Declaration.
+				--  For example, in C this would take you to the header
+				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+			end,
+		})
 	end,
 }
